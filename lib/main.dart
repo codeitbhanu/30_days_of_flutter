1d3c658dbc90d1d5a7b7c57f6ba2181621232386
codeitbhanu <codeitbhanu@gmail.com>
2021-05-27 23:49:48 +0530

    Added routes to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_30_days_of_flutter/pages/home_page.dart';
+import 'package:flutter_30_days_of_flutter/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       darkTheme: ThemeData(brightness: Brightness.light),
+      routes: {
+        "/": (context) => HomePage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+      initialRoute: "/home",
     );
   }
 }
